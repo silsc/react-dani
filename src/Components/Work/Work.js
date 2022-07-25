37915b990617f101e1f2d25e8664a6ea77f48f12
Silvia Salat <salat.silvia@gmail.com>
2022-07-25 22:33:56 +0100

    ADD(work section fade effect)

--- src/Components/Work/Work.js
+++ src/Components/Work/Work.js
@@ -2,6 +2,7 @@ import './work.scss';
 import React,{ useState, useEffect } from 'react';
 import { useTranslation } from "react-i18next";
 import { useInView } from "react-intersection-observer";
+import { Fade } from "react-awesome-reveal";
 import Header from '../Header/Header';
 import Player from '../Player/Player';
 import { ReactComponent as Spotify } from '../../assets/icons/spotify.svg';
@@ -17,8 +18,12 @@ const Work = React.forwardRef((props, ref) => {
     <section ref={ref} className={`work section ${ props.inView ? "theme--dark" : "theme--default"}`} id="work">
       <Header/>
       <div className='work-content'>
-        <Player/>
+        <Fade delay={200}>
+          <Player/>
+        </Fade>
         <div className='work-info'>
+        <Fade delay={200}>
+
           <h2 className='work-title'>{t("work.title")}</h2>
           <p>
           Do you want to listen to some more of my projects?</p>
@@ -29,6 +34,7 @@ const Work = React.forwardRef((props, ref) => {
             <p>Contact me or listen <span onClick={() => window.location = 'https://open.spotify.com/playlist/2nwVcowWfYhXXqkmVgGVNK?si=586a412f544a44e7&nd=1, "_blank"'}>my playlist on Spotify </span></p>
             <Spotify onClick={() => window.location = 'https://open.spotify.com/playlist/2nwVcowWfYhXXqkmVgGVNK?si=586a412f544a44e7&nd=1', "_blank"}></Spotify>
           </div>
+        </Fade>
         </div>
         
       </div>

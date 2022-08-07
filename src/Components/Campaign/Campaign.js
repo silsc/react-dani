f29577ecf46ec8d8407ecc93e74eacf7abed0a1f
Restyled.io <commits@restyled.io>
2022-08-07 20:48:24 +0000

    Restyled by prettier

--- src/Components/Campaign/Campaign.js
+++ src/Components/Campaign/Campaign.js
@@ -1,42 +1,54 @@
-import './campaign.scss';
+import "./campaign.scss";
 
-import React, {useEffect, useRef, useState} from 'react';
-import {useTranslation} from "react-i18next";
-import {InView} from 'react-intersection-observer';
+import React, { useEffect, useRef, useState } from "react";
+import { useTranslation } from "react-i18next";
+import { InView } from "react-intersection-observer";
 
-import Header from '../Header/Header';
-import MailchimpFormContainer from
-    '../MailchimpFormContainer/MailchimpFormContainer';
-import Navbar from '../Navbar/Navbar';
+import Header from "../Header/Header";
+import MailchimpFormContainer from "../MailchimpFormContainer/MailchimpFormContainer";
+import Navbar from "../Navbar/Navbar";
 
 function Campaign() {
-  const {t, i18n} = useTranslation();
-  const changeLanguage = (language) => { i18n.changeLanguage(language); };
+  const { t, i18n } = useTranslation();
+  const changeLanguage = (language) => {
+    i18n.changeLanguage(language);
+  };
   const [inView, setInView] = useState();
 
   useEffect(() => {
-    document.documentElement.classList.add(`${window.innerWidth >= 760 ?(!inView ? "theme--default" : "theme--dark") : "theme--dark" }`)
+    document.documentElement.classList.add(
+      `${
+        window.innerWidth >= 760
+          ? !inView
+            ? "theme--default"
+            : "theme--dark"
+          : "theme--dark"
+      }`
+    );
     return () => {
-    document.documentElement.classList.remove(`${
-        window.innerWidth >= 760 ? (!inView ? "theme--default" : "theme--dark")
-                                 : "theme--dark"}`) }
+      document.documentElement.classList.remove(
+        `${
+          window.innerWidth >= 760
+            ? !inView
+              ? "theme--default"
+              : "theme--dark"
+            : "theme--dark"
+        }`
+      );
+    };
   });
 
-    return (<>
-            <Navbar inView =
-             {
-               inView
-             } />
+  return (
+    <>
+      <Navbar inView={inView} />
 
-    <section className={`campaign section`}>
-      <Header/>
-            <h2 className = 'title'>{
-                t("campaign.header")}</h2>
-      <MailchimpFormContainer />
-            </section>
+      <section className={`campaign section`}>
+        <Header />
+        <h2 className="title">{t("campaign.header")}</h2>
+        <MailchimpFormContainer />
+      </section>
     </>
-
-    );
+  );
 }
 
 export default Campaign;
--- src/index.js
+++ src/index.js
@@ -1,18 +1,18 @@
 // import reportWebVitals from './reportWebVitals';
-import './i18n';
+import "./i18n";
 import "../src/index.css";
 
-import React, {Suspense, useEffect} from 'react';
-import ReactDOM from 'react-dom';
-import {createRoot} from 'react-dom/client';
-import ReactGA from 'react-ga';
-import {BrowserRouter, Route, Routes} from "react-router-dom";
+import React, { Suspense, useEffect } from "react";
+import ReactDOM from "react-dom";
+import { createRoot } from "react-dom/client";
+import ReactGA from "react-ga";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 
-import App from './App';
-import Campaign from './Components/Campaign/Campaign';
-import Policy from './Components/Policy/Policy';
+import App from "./App";
+import Campaign from "./Components/Campaign/Campaign";
+import Policy from "./Components/Policy/Policy";
 
-const rootElement = document.getElementById('root');
+const rootElement = document.getElementById("root");
 const root = createRoot(rootElement);
 
 ReactGA.initialize(`${process.env.REACT_APP_GA_KEY}`);
@@ -23,16 +23,16 @@ root.render(
     <BrowserRouter>
       <Suspense fallback={<div>Loading...</div>}>
         <Routes>
-          <Route exact path="/" element={<App/>}/>
-          <Route path="/policy" element={<Policy/>}/>
-          <Route path="/form" element={<Campaign/>}/>
+          <Route exact path="/" element={<App />} />
+          <Route path="/policy" element={<Policy />} />
+          <Route path="/form" element={<Campaign />} />
         </Routes>
       </Suspense>
     </BrowserRouter>
-  </React.StrictMode>,
+  </React.StrictMode>
 );
 
-  // If you want to start measuring performance in your app, pass a function
-  // to log results (for example: reportWebVitals(console.log))
-  // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-  // reportWebVitals();
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+// reportWebVitals();

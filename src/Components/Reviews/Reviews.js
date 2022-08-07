255adb521814ad3ad5474c8a3710715ab8069b8d
Restyled.io <commits@restyled.io>
2022-08-07 15:42:46 +0000

    Restyled by clang-format

--- src/Components/Reviews/Reviews.js
+++ src/Components/Reviews/Reviews.js
@@ -1,50 +1,51 @@
 import './reviews.scss';
-import { useState } from 'react';
-import { useTranslation } from "react-i18next";
-import { Fade } from "react-awesome-reveal";
-import Header from '../Header/Header';
+
+import {useState} from 'react';
+import {Fade} from "react-awesome-reveal";
+import {useTranslation} from "react-i18next";
+
 import chanderImg from "../../assets/images/chander.jpeg";
 import dezImg from "../../assets/images/dez.jpeg";
-import marcImg from "../../assets/images/marc.jpeg";
 import kirianImg from "../../assets/images/kirian.jpeg";
+import marcImg from "../../assets/images/marc.jpeg";
 import perarniImg from "../../assets/images/perarni.jpeg";
-
+import Header from '../Header/Header';
 
 function Reviews() {
-  const { t, i18n } = useTranslation();
+  const {t, i18n} = useTranslation();
 
-  const [ darkTheme, setDarkTheme ] = useState(false)
+  const [darkTheme, setDarkTheme] = useState(false)
 
   const testimonials = [
     {
-      name: t("reviews.card-1.name"),
-      text: t("reviews.card-1.review"),
-      band: t("reviews.card-1.band"),
-      image: dezImg
+      name : t("reviews.card-1.name"),
+      text : t("reviews.card-1.review"),
+      band : t("reviews.card-1.band"),
+      image : dezImg
     },
     {
-      name: t("reviews.card-2.name"),
-      text: t("reviews.card-2.review"),
-      band: t("reviews.card-2.band"),
-      image: kirianImg
+      name : t("reviews.card-2.name"),
+      text : t("reviews.card-2.review"),
+      band : t("reviews.card-2.band"),
+      image : kirianImg
     },
     {
-      name: t("reviews.card-3.name"),
-      text: t("reviews.card-3.review"),
-      band: t("reviews.card-3.band"),
-      image: chanderImg
+      name : t("reviews.card-3.name"),
+      text : t("reviews.card-3.review"),
+      band : t("reviews.card-3.band"),
+      image : chanderImg
     },
     {
-      name: t("reviews.card-4.name"),
-      text: t("reviews.card-4.review"),
-      band: t("reviews.card-4.band"),
-      image: perarniImg
+      name : t("reviews.card-4.name"),
+      text : t("reviews.card-4.review"),
+      band : t("reviews.card-4.band"),
+      image : perarniImg
     },
     {
-      name: t("reviews.card-5.name"),
-      text: t("reviews.card-5.review"),
-      band: t("reviews.card-5.band"),
-      image: marcImg
+      name : t("reviews.card-5.name"),
+      text : t("reviews.card-5.review"),
+      band : t("reviews.card-5.band"),
+      image : marcImg
     }
   ];
 
@@ -52,27 +53,23 @@ function Reviews() {
   const [touchEnd, setTouchEnd] = useState(0);
   const [check, setCheck] = useState(false)
 
-  function handleTouchStart(e) {
-      setTouchStart(e.targetTouches[0].clientX);
-  }
+  function handleTouchStart(e) { setTouchStart(e.targetTouches[0].clientX); }
 
-  function handleTouchMove(e) {
-      setTouchEnd(e.targetTouches[0].clientX);
-  }
+  function handleTouchMove(e) { setTouchEnd(e.targetTouches[0].clientX); }
 
   function handleTouchEnd(index) {
-      if (touchStart - touchEnd > 0) {
-        const nextEl =  document.getElementById(`t-${index + 2}`);
-        setCheck(true)
-        nextEl.checked = setCheck;
-        ;
-      }
+    if (touchStart - touchEnd > 0) {
+      const nextEl = document.getElementById(`t-${index + 2}`);
+      setCheck(true)
+      nextEl.checked = setCheck;
+      ;
+    }
 
-      if (touchStart - touchEnd < -1) {
-        const prevEl = document.getElementById(`t-${index}`);
-        setCheck(true)
-        prevEl.checked = setCheck;
-      }
+    if (touchStart - touchEnd < -1) {
+      const prevEl = document.getElementById(`t-${index}`);
+      setCheck(true)
+      prevEl.checked = setCheck;
+    }
   }
 
   return (
@@ -91,26 +88,29 @@ function Reviews() {
             <div className="testimonials">
               {testimonials.map((testimonial, index) => {
                 return (
-                  <label 
-                    className="testimonial" 
-                    htmlFor={"t-"+(index + 1)} 
-                    id={"testimonial-"+(index + 1)}
-                    onTouchStart={touchStartEvent => handleTouchStart(touchStartEvent)}
-                    onTouchMove={touchMoveEvent =>handleTouchMove(touchMoveEvent)}
-                    onTouchEnd={() => handleTouchEnd(index)}>
-                    <img src={testimonial.image} className='testimonial-img'/>
-                    <div className="testimonial-container">
-                      <p className="testimonial-text">{testimonial.text}</p>
+                  <label
+                className = "testimonial"
+    htmlFor = {"t-" + (index + 1)} id = {"testimonial-" +
+                                         (index + 1)} onTouchStart =
+        {touchStartEvent => handleTouchStart(
+             touchStartEvent)} onTouchMove = {touchMoveEvent =>
+                                                  handleTouchMove(
+                                                      touchMoveEvent)} onTouchEnd =
+            {() => handleTouchEnd(index)} >
+            <img src = {testimonial.image} className = 'testimonial-img' />
+            <div className = "testimonial-container">
+            <p className = "testimonial-text">{testimonial.text}<
+                /p>
                       <h1 className="testimonial-name">{testimonial.name}</h1>
-                      <h3 className="testimonial-band">{testimonial.band}</h3>
+            <h3 className = "testimonial-band">{
+                testimonial.band}</h3>
                     </div>
-                  </label>
+            </label>
                 );
               })}
             </div>
-        </div>
-      </Fade>
-    </section>
+            </div>
+      </Fade>< /section>
   );
 }
 

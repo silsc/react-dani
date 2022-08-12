0a08109e51c4bcb42a187c44f44d6e6e156b71d5
Restyled.io <commits@restyled.io>
2022-08-12 07:20:14 +0000

    Restyled by prettier

--- src/Components/CustomForm/CustomForm.js
+++ src/Components/CustomForm/CustomForm.js
@@ -1,80 +1,95 @@
-import './customform.scss';
+import "./customform.scss";
 
-import React, {useEffect, useState} from 'react';
-import {useTranslation} from "react-i18next";
+import React, { useEffect, useState } from "react";
+import { useTranslation } from "react-i18next";
 import MailchimpSubscribe from "react-mailchimp-subscribe";
-import {Link} from "react-router-dom";
+import { Link } from "react-router-dom";
 
-const CustomForm = ({status, message, onValidated}) => {
-    const { t, i18n } = useTranslation();
-    const changeLanguage = (language) => {
-      i18n.changeLanguage(language);
-    }
-    const [email, setEmail] = useState('');
+const CustomForm = ({ status, message, onValidated }) => {
+  const { t, i18n } = useTranslation();
+  const changeLanguage = (language) => {
+    i18n.changeLanguage(language);
+  };
+  const [email, setEmail] = useState("");
 
-    const handleSubmit = (e) => {
-      e.preventDefault();
-      email &&
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    email &&
       email.indexOf("@") > -1 &&
       onValidated({
-          EMAIL: email,
+        EMAIL: email,
       });
+  };
+
+  const validateInput = (values) => {
+    if (values.some((f) => f === "") || values[0].indexOf("@") === -1) {
+      return true;
+    } else {
+      return false;
     }
+  };
 
-    const validateInput = values => {
+  useEffect(() => {
+    if (status === "success") clearFields();
+  }, [status]);
 
-      if (values.some(f => f === "") || values[0].indexOf("@") === -1) {
-          return true
-      } else {
-          return false
-      }
-    }
+  const clearFields = () => {
+    setEmail("");
+  };
 
-    useEffect(() => {
-      if(status === "success") clearFields();
-    }, [status])
+  return (
+    <form className="mc__form" onSubmit={(e) => handleSubmit(e)}>
+      {status === "success" ? (
+        <div>
+          <h3 className="text sent">{t("campaign.sent-1")}</h3>
+          <h3 className="text sent">{t("campaign.sent-2")}</h3>
+        </div>
+      ) : (
+        <h3 className="text">{t("campaign.text")}</h3>
+      )}
 
-    const clearFields = () => {
-        setEmail('');
-    }
+      {status === "sending" && (
+        <div className="alert mc__alert--sending">{t("campaign.sending")}</div>
+      )}
 
-    return (
-      <form className="mc__form" onSubmit={(e) => handleSubmit(e)}>
-        
-        {status === "success" ? <div><h3 className="text sent">{t("campaign.sent-1")}</h3><h3 className="text sent">{t("campaign.sent-2")}</h3></div> : <h3 className="text">{t("campaign.text")}</h3> 
-        }
-        
-        {status === "sending" && (
-          <div className="alert mc__alert--sending">{t("campaign.sending")}</div>
-        )}
-
-        {status === "error" && (
-          <div className="alert mc__alert--error" dangerouslySetInnerHTML={{ __html: message }} />
-        )}
-
-        {status !== "success" ? (
-          <div className="mc__field-container">
-
-            <input
-              onChange={(e) => setEmail(e.target.value)}
-              label="Email"
-              type="email"
-              placeholder="your@email.com"
-              defaultValue={email}
-              required
-            />
-
-          </div>
-          ) : null
-        }
-        <Link to="policy" className='policy-btn'> {t("campaign.privacy")}</Link> 
-        { status === 'success' ? <Link to="/" className='link-web'>{t("campaign.goback")}</Link>  : 
-        <input className="submit-btn" type='submit' defaultValue="submit" value={t("campaign.send")} disabled={validateInput([email])}
+      {status === "error" && (
+        <div
+          className="alert mc__alert--error"
+          dangerouslySetInnerHTML={{ __html: message }}
         />
-        }
+      )}
 
-      </form>
-    );
+      {status !== "success" ? (
+        <div className="mc__field-container">
+          <input
+            onChange={(e) => setEmail(e.target.value)}
+            label="Email"
+            type="email"
+            placeholder="your@email.com"
+            defaultValue={email}
+            required
+          />
+        </div>
+      ) : null}
+      <Link to="policy" className="policy-btn">
+        {" "}
+        {t("campaign.privacy")}
+      </Link>
+      {status === "success" ? (
+        <Link to="/" className="link-web">
+          {t("campaign.goback")}
+        </Link>
+      ) : (
+        <input
+          className="submit-btn"
+          type="submit"
+          defaultValue="submit"
+          value={t("campaign.send")}
+          disabled={validateInput([email])}
+        />
+      )}
+    </form>
+  );
 };
 
 export default CustomForm;
--- src/Components/Work/Work.js
+++ src/Components/Work/Work.js
@@ -1,48 +1,74 @@
-import './work.scss';
+import "./work.scss";
 
-import React, {useEffect, useState} from 'react';
-import {Fade} from "react-awesome-reveal";
-import {useTranslation} from "react-i18next";
-import {useInView} from "react-intersection-observer";
-import {HashLink} from 'react-router-hash-link';
+import React, { useEffect, useState } from "react";
+import { Fade } from "react-awesome-reveal";
+import { useTranslation } from "react-i18next";
+import { useInView } from "react-intersection-observer";
+import { HashLink } from "react-router-hash-link";
 
-import {ReactComponent as Spotify} from '../../assets/icons/spotify.svg';
-import Header from '../Header/Header';
-import Player from '../Player/Player';
+import { ReactComponent as Spotify } from "../../assets/icons/spotify.svg";
+import Header from "../Header/Header";
+import Player from "../Player/Player";
 
 const Work = React.forwardRef((props, ref) => {
-  const {t, i18n} = useTranslation();
-  const changeLanguage = (language) => { i18n.changeLanguage(language); }
+  const { t, i18n } = useTranslation();
+  const changeLanguage = (language) => {
+    i18n.changeLanguage(language);
+  };
 
-  const openInNewTab =
-      url => { window.open(url, '_blank', 'noopener,noreferrer'); };
+  const openInNewTab = (url) => {
+    window.open(url, "_blank", "noopener,noreferrer");
+  };
 
   return (
-    <section ref={ref} className={`work section ${ props.inView ? "theme--dark" : "theme--default"}`} id="work">
-      <Header/>
-      <div className='work-content'>
+    <section
+      ref={ref}
+      className={`work section ${
+        props.inView ? "theme--dark" : "theme--default"
+      }`}
+      id="work"
+    >
+      <Header />
+      <div className="work-content">
         <Fade delay={200}>
-          <Player/>
+          <Player />
         </Fade>
-        <div className='work-info'>
+        <div className="work-info">
           <Fade cascade triggerOnce delay={200}>
-            <h2 className='work-title'>{t("work.title")}</h2>
+            <h2 className="work-title">{t("work.title")}</h2>
             <p>{t("work.text-1")}</p>
             <p>{t("work.text-2")}</p>
 
-            <div className='work-spotify'>
+            <div className="work-spotify">
               <p>
-              <HashLink className='bold link' smooth to="/#contact">
-              {t("work.cta-1")}
-              </HashLink>
-              {t("work.text-3")} <span className='cursor spotify-green bold' onClick={() => openInNewTab('https://open.spotify.com/playlist/2nwVcowWfYhXXqkmVgGVNK?si=586a412f544a44e7&nd=1')}>{t("work.cta-2")} </span></p>
-              <Spotify onClick={() => openInNewTab('https://open.spotify.com/playlist/2nwVcowWfYhXXqkmVgGVNK?si=586a412f544a44e7&nd=1')}></Spotify>
+                <HashLink className="bold link" smooth to="/#contact">
+                  {t("work.cta-1")}
+                </HashLink>
+                {t("work.text-3")}{" "}
+                <span
+                  className="cursor spotify-green bold"
+                  onClick={() =>
+                    openInNewTab(
+                      "https://open.spotify.com/playlist/2nwVcowWfYhXXqkmVgGVNK?si=586a412f544a44e7&nd=1"
+                    )
+                  }
+                >
+                  {t("work.cta-2")}{" "}
+                </span>
+              </p>
+              <Spotify
+                onClick={() =>
+                  openInNewTab(
+                    "https://open.spotify.com/playlist/2nwVcowWfYhXXqkmVgGVNK?si=586a412f544a44e7&nd=1"
+                  )
+                }
+              ></Spotify>
             </div>
           </Fade>
         </div>
       </div>
     </section>
   );
-})
+});
 
 export default Work;

12248d5030a3102196dfe954a13d07a71bdc9043
Restyled.io <commits@restyled.io>
2024-10-03 11:02:42 +0000

    Restyled by prettier

--- src/Components/Reviews/Reviews.js
+++ src/Components/Reviews/Reviews.js
@@ -1,113 +1,133 @@
-import './reviews.scss';
+import "./reviews.scss";
 
-import {useState} from 'react';
-import {Fade} from "react-awesome-reveal";
-import {useTranslation} from "react-i18next";
+import { useState } from "react";
+import { Fade } from "react-awesome-reveal";
+import { useTranslation } from "react-i18next";
 
 import chanderImg from "../../assets/images/chander.jpeg";
 import dezImg from "../../assets/images/dez.jpeg";
 import kirianImg from "../../assets/images/kirian.png";
 import marcImg from "../../assets/images/marc.png";
 import perarniImg from "../../assets/images/perarni.jpeg";
-import Header from '../Header/Header';
+import Header from "../Header/Header";
 
 function Reviews() {
-  const {t, i18n} = useTranslation();
+  const { t, i18n } = useTranslation();
 
-  const [darkTheme, setDarkTheme] = useState(false)
+  const [darkTheme, setDarkTheme] = useState(false);
 
   const testimonials = [
     {
-      name : t("reviews.card-1.name"),
-      text : t("reviews.card-1.review"),
-      band : t("reviews.card-1.band"),
-      image : dezImg
+      name: t("reviews.card-1.name"),
+      text: t("reviews.card-1.review"),
+      band: t("reviews.card-1.band"),
+      image: dezImg,
     },
     {
-      name : t("reviews.card-2.name"),
-      text : t("reviews.card-2.review"),
-      band : t("reviews.card-2.band"),
-      image : kirianImg
+      name: t("reviews.card-2.name"),
+      text: t("reviews.card-2.review"),
+      band: t("reviews.card-2.band"),
+      image: kirianImg,
     },
     {
-      name : t("reviews.card-3.name"),
-      text : t("reviews.card-3.review"),
-      band : t("reviews.card-3.band"),
-      image : chanderImg
+      name: t("reviews.card-3.name"),
+      text: t("reviews.card-3.review"),
+      band: t("reviews.card-3.band"),
+      image: chanderImg,
     },
     {
-      name : t("reviews.card-4.name"),
-      text : t("reviews.card-4.review"),
-      band : t("reviews.card-4.band"),
-      image : perarniImg
+      name: t("reviews.card-4.name"),
+      text: t("reviews.card-4.review"),
+      band: t("reviews.card-4.band"),
+      image: perarniImg,
     },
     {
-      name : t("reviews.card-5.name"),
-      text : t("reviews.card-5.review"),
-      band : t("reviews.card-5.band"),
-      image : marcImg
-    }
+      name: t("reviews.card-5.name"),
+      text: t("reviews.card-5.review"),
+      band: t("reviews.card-5.band"),
+      image: marcImg,
+    },
   ];
 
   const [touchStart, setTouchStart] = useState(0);
   const [touchEnd, setTouchEnd] = useState(0);
-  const [check, setCheck] = useState(false)
+  const [check, setCheck] = useState(false);
 
-  function handleTouchStart(e) { setTouchStart(e.targetTouches[0].clientX); }
+  function handleTouchStart(e) {
+    setTouchStart(e.targetTouches[0].clientX);
+  }
 
-  function handleTouchMove(e) { setTouchEnd(e.targetTouches[0].clientX); }
+  function handleTouchMove(e) {
+    setTouchEnd(e.targetTouches[0].clientX);
+  }
 
   function handleTouchEnd(index) {
     if (touchStart - touchEnd > 0) {
       const nextEl = document.getElementById(`t-${index + 2}`);
-      setCheck(true)
+      setCheck(true);
       nextEl.checked = setCheck;
-      ;
     }
 
     if (touchStart - touchEnd < -1) {
       const prevEl = document.getElementById(`t-${index}`);
-      setCheck(true)
+      setCheck(true);
       prevEl.checked = setCheck;
     }
   }
 
-  return (<section className = {`reviews section ${
-               !darkTheme ? "theme--default" : "theme--dark"}`} id = "reviews">
-          <Fade><h2 className = 'reviews-title title'>{t("reviews.title")} <
-          /h2>
-      </Fade > <Fade delay = {300}><div className = "slider">
-          <input type = "radio" name = "testimonial" id = "t-1" />
-          <input type = "radio" name = "testimonial" id = "t-2" defaultChecked =
-           {
-             window.innerWidth >= 760 ? "true" : "false"
-           } />
+  return (
+    <section
+      className={`reviews section ${
+        !darkTheme ? "theme--default" : "theme--dark"
+      }`}
+      id="reviews"
+    >
+      <Fade>
+        <h2 className="reviews-title title">{t("reviews.title")} </h2>
+      </Fade>{" "}
+      <Fade delay={300}>
+        <div className="slider">
+          <input type="radio" name="testimonial" id="t-1" />
+          <input
+            type="radio"
+            name="testimonial"
+            id="t-2"
+            defaultChecked={window.innerWidth >= 760 ? "true" : "false"}
+          />
           <input type="radio" name="testimonial" id="t-3" />
-          <input type = "radio" name = "testimonial" id = "t-4" />
-          <input type = "radio" name = "testimonial" id = "t-5" />
-          <div className = "testimonials">{
-              testimonials.map((testimonial, index) => {
-                return (
-                  <label
-                className = "testimonial"
-                    htmlFor={"t-"+(index + 1)} 
-                    id={"testimonial-"+(index + 1)}
-                    onTouchStart={touchStartEvent => handleTouchStart(touchStartEvent)}
-                    onTouchMove={touchMoveEvent =>handleTouchMove(touchMoveEvent)}
-                    onTouchEnd={() => handleTouchEnd(index)}>
-                    <img src={testimonial.image} className='testimonial-img'/>
-                    <Fade >
+          <input type="radio" name="testimonial" id="t-4" />
+          <input type="radio" name="testimonial" id="t-5" />
+          <div className="testimonials">
+            {testimonials.map((testimonial, index) => {
+              return (
+                <label
+                  className="testimonial"
+                  htmlFor={"t-" + (index + 1)}
+                  id={"testimonial-" + (index + 1)}
+                  onTouchStart={(touchStartEvent) =>
+                    handleTouchStart(touchStartEvent)
+                  }
+                  onTouchMove={(touchMoveEvent) =>
+                    handleTouchMove(touchMoveEvent)
+                  }
+                  onTouchEnd={() => handleTouchEnd(index)}
+                >
+                  <img src={testimonial.image} className="testimonial-img" />
+                  <Fade>
                     <div className="testimonial-container">
                       <p className="testimonial-text">{testimonial.text}</p>
                       <h1 className="testimonial-name">{testimonial.name}</h1>
                       <h3 className="testimonial-band">{testimonial.band}</h3>
                     </div>
-                    </Fade>
-                  </label>
-                );
-              })} < /div>
-        </div > </Fade>
-    </section>);
+                  </Fade>
+                </label>
+              );
+            })}{" "}
+          </div>
+        </div>{" "}
+      </Fade>
+    </section>
+  );
 }
 
 export default Reviews;
--- src/Components/Work/Work.js
+++ src/Components/Work/Work.js
@@ -1,45 +1,77 @@
-import './work.scss';
+import "./work.scss";
 
-import React, {useEffect, useState} from 'react';
-import {Fade} from "react-awesome-reveal";
-import {useTranslation} from "react-i18next";
-import {useInView} from "react-intersection-observer";
-import {HashLink} from 'react-router-hash-link';
+import React, { useEffect, useState } from "react";
+import { Fade } from "react-awesome-reveal";
+import { useTranslation } from "react-i18next";
+import { useInView } from "react-intersection-observer";
+import { HashLink } from "react-router-hash-link";
 
-import {ReactComponent as Spotify} from '../../assets/icons/spotify.svg';
-import Header from '../Header/Header';
-import Player from '../Player/Player';
+import { ReactComponent as Spotify } from "../../assets/icons/spotify.svg";
+import Header from "../Header/Header";
+import Player from "../Player/Player";
 
 const Work = React.forwardRef((props, ref) => {
-  const {t, i18n} = useTranslation();
-  const changeLanguage = (language) => { i18n.changeLanguage(language); }
+  const { t, i18n } = useTranslation();
+  const changeLanguage = (language) => {
+    i18n.changeLanguage(language);
+  };
 
-  const openInNewTab =
-      url => { window.open(url, '_blank', 'noopener,noreferrer'); };
+  const openInNewTab = (url) => {
+    window.open(url, "_blank", "noopener,noreferrer");
+  };
 
-  useEffect(() => {console.log(window.location.pathname)});
+  useEffect(() => {
+    console.log(window.location.pathname);
+  });
 
   return (
-    <section ref={ref} className={`work section ${ props.inView ? "theme--dark" : "theme--default"}`} id="work">
-      <div className='work-content'>
+    <section
+      ref={ref}
+      className={`work section ${props.inView ? "theme--dark" : "theme--default"}`}
+      id="work"
+    >
+      <div className="work-content">
         <Fade delay={200}>
-          <Player/>
+          <Player />
         </Fade>
-        <div className='work-info'>
+        <div className="work-info">
           <Fade cascade triggerOnce delay={200}>
-            <h2 className='work-title'>{t("work.title")}</h2>
+            <h2 className="work-title">{t("work.title")}</h2>
             <p>{t("work.text-1")}</p>
-            <p>{t("work.text-2")}
-              <HashLink className='bold link' smooth to={window.location.pathname === "/" ? "/#contact" : ""}>
-              {t("work.cta-1")}
+            <p>
+              {t("work.text-2")}
+              <HashLink
+                className="bold link"
+                smooth
+                to={window.location.pathname === "/" ? "/#contact" : ""}
+              >
+                {t("work.cta-1")}
               </HashLink>
-              {t("work.text-3")} <span className='cursor spotify-green bold' onClick={() => openInNewTab('https://open.spotify.com/playlist/2nwVcowWfYhXXqkmVgGVNK?si=586a412f544a44e7&nd=1')}>{t("work.cta-2")} </span></p>
-              <Spotify className="work--spotify" onClick={() => openInNewTab('https://open.spotify.com/playlist/2nwVcowWfYhXXqkmVgGVNK?si=586a412f544a44e7&nd=1')}></Spotify>
+              {t("work.text-3")}{" "}
+              <span
+                className="cursor spotify-green bold"
+                onClick={() =>
+                  openInNewTab(
+                    "https://open.spotify.com/playlist/2nwVcowWfYhXXqkmVgGVNK?si=586a412f544a44e7&nd=1",
+                  )
+                }
+              >
+                {t("work.cta-2")}{" "}
+              </span>
+            </p>
+            <Spotify
+              className="work--spotify"
+              onClick={() =>
+                openInNewTab(
+                  "https://open.spotify.com/playlist/2nwVcowWfYhXXqkmVgGVNK?si=586a412f544a44e7&nd=1",
+                )
+              }
+            ></Spotify>
           </Fade>
         </div>
       </div>
     </section>
   );
-})
+});
 
 export default Work;
